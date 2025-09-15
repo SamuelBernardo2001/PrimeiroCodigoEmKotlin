00f7368aeff1fcf112d7cf8574b78e446eefbc33
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-15 01:09:41 -0300

    no Segundo Codigo Foi Aprendido sobre as variaveis constantes, tanto global, mutavel e imutavel, tbm foi feito uns teste com calculos no codigo

--- app/src/main/java/com/example/primeiro_codigo_em_kotlin/VariaveisConstantes.kt
+++ app/src/main/java/com/example/primeiro_codigo_em_kotlin/VariaveisConstantes.kt
@@ -1,19 +1,35 @@
 
 
-
-
+//                              Primeiros codigos.
+//    println("Hello. world!")
+//                                  Constante global, definido na compilação   \/
+//    const val n = "Larissa"       Constante em tempo de compilação (o valor precisa ser conhecido ANTES de rodar).
+//    var nome: String              (Ja iniciando o tipo da variavel)
+//    var nome1 = "Samuel"          variável mutável
+//    val nome2 = "Bernardo"        variável imutável (constante)
+//    val ano = 2025                variável imutável (constante)
+//    var mes = 9                   variável mutável
+//    val dia = 14                  variável imutável (constante)
+//
+//    println("Bem Vindo, " + nome1 + " " + nome2 + ", Estamos em " + ano + "/" + mes + "/" + dia)
+//    println("Primeiro Codigo Em Kotlin Iniciado, $nome1 $nome2 !")
+//
+//                                Segundo Codigos
+//    val num1 = 15
+//    val num2 = 5
+//
+//    val soma = num1 + num2
+//    val divisao = num1 / num2
+//    val subtracao = num1 - num2
+//    val multiplication = num1 * num2
+//
+//    println("a soma do valor De $num1 + $num2 é $soma")
+//    println("a subtração do valor De $num1 + $num2 é $subtracao")
+//    println("a divisão do valor De $num1 + $num2 é $divisao")
+//    println("a multiplicação do valor De $num1 + $num2 é $multiplication")
+//
+//    println("O Valor Total Da (Soma,Subtração,divisão e multiplicação) é ${soma+divisao+subtracao+multiplication}")
 
 fun main() {
-
-    println("Hello. world!")
-
-    var nome1 = "Samuel"
-    val nome2 = "Bernardo"
-    val ano = 2025
-    var mes = 9
-    val dia = 14
-
-    println("Bem Vindo, " + nome1 + " " + nome2 + ", Estamos em " + ano + "/" + mes + "/" + dia)
-    println("Primeiro Codigo Em Kotlin Iniciado, $nome1 $nome2 !")
 
 }
