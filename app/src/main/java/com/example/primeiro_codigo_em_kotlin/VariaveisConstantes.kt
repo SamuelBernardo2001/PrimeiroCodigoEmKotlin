ff9d75c0b3a650197c1536906fdbd78b08373677
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-09-14 23:20:25 -0300

    iniciando um file com o nome de VariaveisConstantes, e criados as primeiras variaveis e os primeiros codigos em Kotlin!

---
+++ app/src/main/java/com/example/primeiro_codigo_em_kotlin/VariaveisConstantes.kt
@@ -0,0 +1,19 @@
+
+
+
+
+
+fun main() {
+
+    println("Hello. world!")
+
+    var nome1 = "Samuel"
+    val nome2 = "Bernardo"
+    val ano = 2025
+    var mes = 9
+    val dia = 14
+
+    println("Bem Vindo, " + nome1 + " " + nome2 + ", Estamos em " + ano + "/" + mes + "/" + dia)
+    println("Primeiro Codigo Em Kotlin Iniciado, $nome1 $nome2 !")
+
+}
